8d94041caeccee8dbf9ed899561d6ae19bbe1f5a
TerrellAW <Terrell.AlexisWilson@edu.sait.ca>
2024-11-09 14:41:35 -0700

    Add project files.

---
+++ ManagementSystem/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ManagementSystem
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ ManagementSystem/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ManagementSystem
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
