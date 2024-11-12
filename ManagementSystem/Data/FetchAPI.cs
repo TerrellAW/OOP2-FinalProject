274c26f452e13fbff524e3cee1f6630d403c4d52
TerrellAW <Terrell.AlexisWilson@edu.sait.ca>
2024-11-11 20:04:53 -0700

    Implemented API fetching
    - FetchAPI.cs contains method for fetching and parsing JSON file from VisualCrossing API
    - DotNetEnv library added to make .env file work
    - Home.cs calls FetchAPI on initialization and displays debug info for troubleshooting

---
+++ ManagementSystem/Data/FetchAPI.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ManagementSystem.Data
+{
+    internal class FetchAPI
+    {
+        // Lists
+        internal static List<Weather> weatherList = new List<Weather>();
+
+        // Methods
+        internal static async Task FetchWeather(string apiUrl)
+        {
+            using var client = new HttpClient();
+
+            try
+            {
+                // Get the weather data from VisualCrossing API
+                var request = new HttpRequestMessage(HttpMethod.Get, apiUrl);
+
+                var response = await client.SendAsync(request);
+                response.EnsureSuccessStatusCode();
+
+                var weatherJson = await response.Content.ReadAsStringAsync();
+
+                if (!string.IsNullOrEmpty(weatherJson))
+                {
+                    // Parse the JSON data into a dynamic object
+                    var weatherData = JsonSerializer.Deserialize<JsonElement>(weatherJson);
+
+                    if (weatherData.TryGetProperty("days", out JsonElement daysElement) && daysElement.ValueKind == JsonValueKind.Array)
+                    {
+                        // Get necessary data from the dynamic object and store in Weather object
+                        foreach (var day in daysElement.EnumerateArray())
+                        {
+                            string datetime = day.GetProperty("datetime").GetString();
+                            string temp = day.GetProperty("temp").GetDouble().ToString();
+                            string description = day.GetProperty("description").GetString();
+
+                            Weather weather = new Weather(datetime, temp, description);
+                            weatherList.Add(weather);
+                        }
+
+                        // Confirm data is in the list
+                        foreach(var weather in weatherList)
+                        {
+                            Debug.WriteLine(weather.ToString());
+                        }
+                    }
+                    else
+                    {
+                        Debug.WriteLine("Error: Could not find 'days' array in JSON data.");
+                    }
+                }
+                else
+                {
+                    Debug.WriteLine("Error: Could not retrieve weather data from API.");
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                Debug.WriteLine($"Error fetching weather data: {e.Message}");
+            }
+        }
+    }
+}
---
+++ ManagementSystem/Data/Weather.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace ManagementSystem.Data
+{
+    internal class Weather
+    {
+        // Fields
+        private string _datetime = "";
+        private string _temp = "";
+        private string _description = "";
+
+        // Properties TODO: Add error handling
+        internal string Datetime
+        {
+            get
+            {
+                return _datetime;
+            }
+            set
+            {
+                _datetime = value;
+            }
+        }
+        internal string Temp
+        {
+            get
+            {
+                return _temp;
+            }
+            set
+            {
+                _temp = value;
+            }
+        }
+        internal string Description
+        {
+            get
+            {
+                return _description;
+            }
+            set
+            {
+                _description = value;
+            }
+        }
+
+        // Constructors
+        internal Weather()
+        {
+        }
+        internal Weather(string Datetime, string Temp, string Description)
+        {
+            this.Datetime = Datetime;
+            this.Temp = Temp;
+            this.Description = Description;
+        }
+
+        // Methods
+        public override string ToString()
+        {
+            return $"Datetime: {Datetime}\n Temp: {Temp}\n Description: {Description}";
+        }
+    }
+}
