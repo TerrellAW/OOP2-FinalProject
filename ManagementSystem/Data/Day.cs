6f9fd8264dbe1b701268313ccedf26c22a48c30d
TerrellAW <Terrell.AlexisWilson@edu.sait.ca>
2024-11-25 10:58:42 -0700

    Created Day class

---
+++ ManagementSystem/Data/Day.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagementSystem.Data
+{
+    internal class Day
+    {
+        // Fields
+        private int _id;
+        private DateTime _date;
+        private List<Event> _events;
+        private Weather _weather;
+
+        // Properties
+    }
+}
