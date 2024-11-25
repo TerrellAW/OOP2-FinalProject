5f2059efd0edcbf794184b60032258aa5fcb62dc
TerrellAW <Terrell.AlexisWilson@edu.sait.ca>
2024-11-25 10:58:49 -0700

    Created Event class

---
+++ ManagementSystem/Data/Event.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagementSystem.Data
+{
+    internal class Event
+    {
+        // Fields
+        private int _id;
+        private string _name;
+        private DateTime _date;
+        private string _location;
+        private string _description;
+
+        // Properties
+        
+    }
+}
