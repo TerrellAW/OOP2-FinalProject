8449e9aa213b5cbb1b19024a2be6f6156a70b902
ShadowNinja909 <Terrell.AlexisWilson@edu.sait.ca>
2024-11-27 23:14:09 -0700

    Created Node

---
+++ ManagementSystem/Data/Node.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagementSystem
+{
+    internal class Node
+    {
+        private object _data;
+        private Node? _next;
+
+        public object Data
+        {
+            get { return _data; }
+            set { _data = value; }
+        }
+
+        public Node? Next
+        {
+            get { return _next; }
+            set { _next = value; }
+        }
+
+        public Node(object data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+}
