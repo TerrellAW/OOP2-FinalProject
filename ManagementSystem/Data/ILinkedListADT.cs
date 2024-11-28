b0e84bfbb26299295da0317b26f8d1d1917ba8da
ShadowNinja909 <Terrell.AlexisWilson@edu.sait.ca>
2024-11-27 23:13:57 -0700

    Added necessary methods to LinkedList Interface

--- ManagementSystem/Data/ILinkedListADT.cs
+++ ManagementSystem/Data/ILinkedListADT.cs
@@ -4,10 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace ManagementSystem
 {
     public interface ILinkedListADT
     {
+        public bool IsEmpty();
         public void Add(object data);
+        public int GetSize();
+        public void GetData(int size);
     }
 }
