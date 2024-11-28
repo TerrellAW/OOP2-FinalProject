a4a2dbb5b255e71ad61caae513ee3389898eb219
ShadowNinja909 <Terrell.AlexisWilson@edu.sait.ca>
2024-11-27 23:14:38 -0700

    Created ObjException

---
+++ ManagementSystem/Exceptions/ObjException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagementSystem.Exceptions
+{
+    internal class ObjException : ApplicationException
+    {
+        public ObjException()
+        {
+        }
+        public ObjException(string message) : base(message)
+        {
+        }
+    }
+
+    internal class NoToStringException : ObjException
+    {
+        public NoToStringException()
+        {
+        }
+        public NoToStringException(string message) : base(message)
+        {
+        }
+    }
+}
