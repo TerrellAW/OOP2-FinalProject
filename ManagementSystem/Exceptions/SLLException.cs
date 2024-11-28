9448c0bc9c325324c35aea918c23687fc1915999
ShadowNinja909 <Terrell.AlexisWilson@edu.sait.ca>
2024-11-27 23:14:46 -0700

    Created SLLException

---
+++ ManagementSystem/Exceptions/SLLException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagementSystem.Exceptions
+{
+    internal class SLLException : ApplicationException
+    {
+        public SLLException()
+        {
+        }
+        public SLLException(string message) : base(message)
+        {
+        }
+    }
+
+    internal class ListNullException : SLLException
+    {
+        public ListNullException()
+        {
+        }
+        public ListNullException(string message) : base(message)
+        {
+        }
+    }
+}
