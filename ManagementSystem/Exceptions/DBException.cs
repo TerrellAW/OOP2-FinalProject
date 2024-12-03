35bb9aafafe8f0bc29d5e181f8103db5e9622376
ShadowNinja909 <Terrell.AlexisWilson@edu.sait.ca>
2024-12-03 15:58:01 -0700

    Created DBException class

---
+++ ManagementSystem/Exceptions/DBException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagementSystem.Exceptions
+{
+    // Exception class for handling database related errors
+    internal class DBException : ApplicationException
+    {
+        public DBException()
+        {
+        }
+        public DBException(string message) : base(message)
+        {
+        }
+    }
+    // Exception for handling database connection errors
+    internal class DBConnectionException : DBException
+    {
+        public DBConnectionException()
+        {
+        }
+        public DBConnectionException(string message) : base(message)
+        {
+        }
+    }
+}
